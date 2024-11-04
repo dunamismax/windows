51ffb32850df15d5859d917b417fe9de3a3028cc
DunamisMax <dunamismax@tutamail.com>
2024-11-04 08:53:45 -0500

    Added FixWeb to the FixOpal batch

--- Batch/Fix Opal.bat
+++ Batch/Fix Opal.bat
@@ -258,6 +258,16 @@ echo done!
 
 echo.
 echo *******************************
+echo Fixing OpalWeb
+echo *******************************
+net stop "W3SVC"
+cd C:\inetpub\wwwroot\OpalWeb
+powershell -command "(Get-Content .\web.config) | Where-Object {$_ -notmatch 'UnhandledExceptionModule'} | Set-Content web.config"
+net start "W3SVC"
+echo done!
+
+echo.
+echo *******************************
 echo Restarting all Opal Services
 echo *******************************
 net stop "Opal Agent"
